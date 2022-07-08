a8da04f56e942f446994bcc40d92c706c472822d
parables <38154990+Parables@users.noreply.github.com>
2022-07-08 06:10:29 +0000

    refactor(Bulk SMS): :recycle: moved into namespace
    ]

--- src/NotificationChannel/ArkeselChannel.php
+++ src/NotificationChannel/ArkeselChannel.php
@@ -45,7 +45,7 @@ class ArkeselChannel
      */
     public function send($notifiable, Notification $notification)
     {
-        if (! method_exists($notification, 'toArkesel')) {
+        if (!method_exists($notification, 'toArkesel')) {
             throw new Exception('"toArkesel($notifiable)" method does not exist');
         }
 
@@ -59,7 +59,7 @@ class ArkeselChannel
             $message = $msg;
         }
 
-        if (! empty($message->recipients)) {
+        if (!empty($message->recipients)) {
             $recipients = is_string($message->recipients) ? explode(',', $message->recipients) : $message->recipients;
         } else {
             $recipients = Arr::wrap($notifiable->routeNotificationFor('arkesel', $notification));
