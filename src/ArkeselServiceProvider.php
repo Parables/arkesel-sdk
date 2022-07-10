52bfa8bf0ff3fa02acb227a12da7a22095179126
parables <38154990+Parables@users.noreply.github.com>
2022-07-10 09:02:23 +0000

    fix(Bulk SMS): :pencil2: fixed namespace typos

--- src/ArkeselServiceProvider.php
+++ src/ArkeselServiceProvider.php
@@ -11,8 +11,8 @@ namespace Parables\ArkeselSdk;
 use Illuminate\Notifications\ChannelManager;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\ServiceProvider;
+use Parables\ArkeselSdk\BulkSms\ArkeselChannel;
 use Parables\ArkeselSdk\BulkSms\SmsClient;
-use Parables\ArkeselSdk\NotificationChannel\ArkeselChannel;
 
 class ArkeselServiceProvider extends ServiceProvider
 {
@@ -21,7 +21,7 @@ class ArkeselServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/arkesel.php', 'arkesel');
+        $this->mergeConfigFrom(__DIR__ . '/../config/arkesel.php', 'arkesel');
 
         // Bind the main class to use with the facade
         $this->app->singleton('sms', function ($app) {
@@ -42,7 +42,7 @@ class ArkeselServiceProvider extends ServiceProvider
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/arkesel.php' => $this->app->configPath('arkesel.php'),
+                __DIR__ . '/../config/arkesel.php' => $this->app->configPath('arkesel.php'),
             ], 'arkesel');
         }
     }
--- src/BulkSms/ArkeselChannel.php
+++ src/BulkSms/ArkeselChannel.php
@@ -6,7 +6,7 @@
  *  @version 1.0.0
  */
 
-namespace Parables\ArkeselSdk\NotificationChannel;
+namespace Parables\ArkeselSdk\BulkSms;
 
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Arr;
@@ -33,7 +33,7 @@ class ArkeselChannel
      */
     public function send($notifiable, Notification $notification)
     {
-        if (! method_exists($notification, 'toArkesel')) {
+        if (!method_exists($notification, 'toArkesel')) {
             throw new  InvalidSmsMessageException(message: '"toArkesel($notifiable)" method does not exist');
         }
 
@@ -42,7 +42,7 @@ class ArkeselChannel
 
         if (is_string($message)) {
             $message = new ArkeselMessage(message: $message);
-        } elseif (! $message instanceof ArkeselMessage) {
+        } elseif (!$message instanceof ArkeselMessage) {
             throw new  InvalidSmsMessageException(
                 message: '"toArkesel($notifiable)" must return either a string or an instance of ArkeselMessage'
             );
